d69584ae4fdcb7b6c233a068f4bd6eb1aaad6be0
Matt Woodstock <58792057+mattwoodstock@users.noreply.github.com>
2025-08-04 17:23:41 -0400

    Update setup.R

--- R/setup.R
+++ R/setup.R
@@ -8,10 +8,10 @@ source("./R_EwE_console_functions.R")
 
 file.console <- "C:/Program Files/EcoSpace Console 1.4.3.1 64-bit/EwEClientConsole.exe" #You'll need to insert the location of your Command-line executable
 
-file.cmdbase <- paste0("./GIST CommandFile_template.txt")
-file.obsts <- paste0("./updated_TS.csv")
-file.predprey <- paste0("./predprey_pairs.csv")
-file.basevul <- paste0("./GIST Vulnerabilities.csv")
+file.cmdbase <- paste0("../data/GIST/GIST CommandFile_template.txt")
+file.obsts <- paste0("../data/GIST/updated_TS.csv")
+file.predprey <- paste0("..data/GIST/predprey_pairs.csv")
+file.basevul <- paste0("..data/GIST/GIST Vulnerabilities.csv")
 
 #Years to test
 startyear    <- 1980
@@ -32,7 +32,6 @@ obs.ts = fn.read_ecosim_timeseries(file.obsts)
 vuls.base <- read.csv(file.basevul,row.names=1)
 group.names <- gsub(" ","_",vuls.base[,1]); group.names
 df.names <- data.frame(group.names = group.names, num = 1:length(group.names)) ## 2 column df with standardized name and pool code
-write.csv(df.names, "./FG_names_standardized.csv", row.names = FALSE)
 
 
 
